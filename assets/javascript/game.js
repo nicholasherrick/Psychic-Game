cb941853b6a1b2ebc73e5a8caebea22f97f7aa29
Nick Herrick <nth.herrick@gmail.com>
2019-10-14 17:19:08 -0400

    added html variable ids, javascript variables, js key press functionallity, js computer guessing math, and code to ensure that only pressing letters will effect the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+// Array that lists all options
+
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+// Variables for wins and losses
+var wins = 0;
+var losses =0;
+
+// variables for html elements
+
+var userGuessesText = document.getElementById("user-guesses");
+
+var userGuessesRemainingText = document.getElementById("guesses-remaining");
+
+var userWinsText = document.getElementById("user-wins");
+
+var userLossesText = document.getElementById("user-losses");
+
+// Key press function
+
+document.onkeyup = function(event) {}
+
+// Determine which key was pressed
+
+var userGuess = event.key;
+
+// Computer randomly choses an option from the options array
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+// Only run the following block if an option is pressed
+
+if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
+
+    userGuessesText.textContent = "Your Guesses so far:" + userGuess;
+    
+}
+
