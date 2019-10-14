dbe470d43b34d5332ef5e0714b3e7d136eec1134
Nick Herrick <nth.herrick@gmail.com>
2019-10-14 17:35:25 -0400

    fixed closing bracket for line 22

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,26 +19,27 @@ var userLossesText = document.getElementById("user-losses");
 
 // Key press function
 
-document.onkeyup = function(event) {}
+document.onkeyup = function(event) {
 
-// Determine which key was pressed
+    // Determine which key was pressed
 
-var userGuess = event.key;
+    var userGuess = event.key;
 
-// Computer randomly choses an option from the options array
+    // Computer randomly choses an option from the options array
 
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-// Only run the following block if an option is pressed
+    // Only run the following block if an option is pressed
 
-if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
+    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
 
-    // Display guesses, guesses remaining, wins, losses
+        // Display guesses, guesses remaining, wins, losses
 
-    userGuessesText.textContent = "Your Guesses so far:" + userGuess;
-    userGuessesRemainingText = "Guesses remaining:" + guesses
-    userWinsText = "Wins:" + wins
-    userLossesText = "Losses:" + losses
+        userGuessesText.textContent = "Your Guesses so far:" + userGuess;
+        userGuessesRemainingText = "Guesses remaining:" + guesses
+        userWinsText = "Wins:" + wins
+        userLossesText = "Losses:" + losses
 
+    }
 }
 
